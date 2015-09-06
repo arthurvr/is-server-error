ae679cbdcce4fb40ebcf88c96fc945c4f3d22150
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-09-06 21:49:57 +0200

    Use AVA for testing

--- test.js
+++ test.js
@@ -1,23 +1,28 @@
 'use strict';
-var assert = require('assert');
+var test = require('ava');
 var isServerError = require('./');
 
 [500, 501, 502, 503, 504, 505, 506, 507, 508, 509, 510, 511, 511, 598, 599].forEach(function (code) {
-	it('should detect `' + code + '`', function () {
-		assert.strictEqual(isServerError(code), true);
+	test('should detect `' + code + '`', function (t) {
+		t.true(isServerError(code));
+		t.end();
 	});
 });
 
-it('should return false for all other codes', function () {
+test('should return false for all other codes', function (t) {
 	[400, 200, 0, 204, 101].forEach(function (code) {
-		assert.strictEqual(isServerError(code), false);
+		t.false(isServerError(code));
 	});
+
+	t.end();
 });
 
-it('should throw on nonsense input', function () {
+test('should throw on nonsense input', function (t) {
 	['hi', function () {}, {}].forEach(function (input) {
-		assert.throws(function () {
+		t.throws(function () {
 			isServerError(input);
 		});
 	});
+
+	t.end();
 });
