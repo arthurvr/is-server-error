034d6b93b7424ea683303051455f7af03901ec3e
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-10-03 17:01:08 +0200

    ES2015ify

--- test.js
+++ test.js
@@ -1,25 +1,24 @@
-'use strict';
-var test = require('ava');
-var isServerError = require('./');
+import test from 'ava';
+import isServerError from './';
 
-[500, 501, 502, 503, 504, 505, 506, 507, 508, 509, 510, 511, 511, 598, 599].forEach(function (code) {
-	test('should detect `' + code + '`', function (t) {
+[500, 501, 502, 503, 504, 505, 506, 507, 508, 509, 510, 511, 511, 598, 599].forEach(code => {
+	test('should detect `' + code + '`', t => {
 		t.true(isServerError(code));
 		t.end();
 	});
 });
 
-test('should return false for all other codes', function (t) {
-	[400, 200, 0, 204, 101].forEach(function (code) {
+test('should return false for all other codes', t => {
+	[400, 200, 0, 204, 101].forEach(code => {
 		t.false(isServerError(code));
 	});
 
 	t.end();
 });
 
-test('should throw on nonsense input', function (t) {
-	['hi', function () {}, {}].forEach(function (input) {
-		t.throws(function () {
+test('should throw on nonsense input', t => {
+	['hi', function () {}, {}].forEach(input => {
+		t.throws(() => {
 			isServerError(input);
 		});
 	});
